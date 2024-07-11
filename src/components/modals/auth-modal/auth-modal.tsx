c4e2080df7e771df6bfb3f5718bd901040f0a9a9
Egor Nikandrov <egr.nikandrov@gmail.com>
2024-07-11 12:02:55 +0300

    1. Сделал Модальное окно 2. Готова валидация в модальном окне 3. Сделал хэдер 4. Лого вынес в отдельный компонент 5. Автодеплой готов

--- src/components/modals/auth-modal/auth-modal.tsx
+++ src/components/modals/auth-modal/auth-modal.tsx
@@ -1,28 +1,177 @@
-import { Button, Placeholder, Modal } from "rsuite";
+import React, { useState } from "react";
+import { Button, Modal, Input, Form } from "rsuite";
+import MainLogo from "./../../main-logo/main-logo.tsx";
+import styles from "./auth-modal.module.scss";
+import { useForm, Controller } from "react-hook-form";
 
 type AuthModalType = {
   open: boolean;
   onClose: () => void;
 };
 
-const AuthModal = ({ open, onClose }: AuthModalType) => {
+type FieldText = {
+  type: "text" | "password";
+  value: string;
+  name: string;
+  onChange: () => void;
+  onBlur: () => void;
+  error?: string;
+  placeholder?: string;
+  size?: "lg" | "md" | "sm" | "xs";
+};
+
+const Field = (props: FieldText) => {
   return (
-    <Modal keyboard={false} open={open} onClose={onClose}>
-      <Modal.Header>
-        <Modal.Title>Modal Title</Modal.Title>
+    <Form.Group>
+      <Input
+        type={props.type}
+        value={props.value}
+        name={props.name}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        placeholder={props.placeholder}
+        size={props.size || "md"}
+      />
+      <Form.ErrorMessage show={!!props.error} placement="bottomStart">
+        {props.error}
+      </Form.ErrorMessage>
+    </Form.Group>
+  );
+};
+
+const AuthModal = ({ open, onClose }: AuthModalType) => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const defaultValues = {
+    email: "",
+    password: "",
+    username: "",
+  };
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues });
+
+  const onSubmit = (data) => {
+    if (isLogin) {
+      console.log("Login data:", data);
+    } else {
+      console.log("Registration data:", data);
+    }
+  };
+
+  return (
+    <Modal
+      keyboard={false}
+      open={open}
+      onClose={onClose}
+      dialogClassName={styles.modalBody}
+      size="sm"
+    >
+      <Modal.Header className={styles.modalHeader}>
+        <Button
+          appearance="subtle"
+          className={`${styles.modeButton} ${isLogin ? styles.active : styles.inactive}`}
+          onClick={() => setIsLogin(true)}
+        >
+          Sign In
+        </Button>
+        <MainLogo className={styles.logo} />
+        <Button
+          appearance="subtle"
+          className={`${styles.modeButton} ${!isLogin ? styles.active : styles.inactive}`}
+          onClick={() => setIsLogin(false)}
+        >
+          Sign Up
+        </Button>
       </Modal.Header>
 
       <Modal.Body>
-        <Placeholder.Paragraph />
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.input}>
+            {!isLogin && (
+              <div className={styles.input}>
+                <Controller
+                  name="username"
+                  rules={{
+                    required: {
+                      value: true,
+                      message: "Please enter your username",
+                    },
+                  }}
+                  control={control}
+                  render={({ field, fieldState }) => (
+                    <Field
+                      {...field}
+                      error={errors[field.name]?.message}
+                      placeholder="Enter your username"
+                      size="sm"
+                    />
+                  )}
+                />
+              </div>
+            )}
+
+            <Controller
+              name="email"
+              rules={{
+                required: {
+                  value: true,
+                  message: "Please enter your email",
+                },
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              }}
+              control={control}
+              render={({ field, fieldState }) => (
+                <Field
+                  {...field}
+                  error={errors[field.name]?.message}
+                  placeholder="Enter your email"
+                  size="sm"
+                />
+              )}
+            />
+          </div>
+
+          <div className={`${styles.input} ${styles.lastInput}`}>
+            <Controller
+              name="password"
+              rules={{
+                required: {
+                  value: true,
+                  message: "Please enter your password",
+                },
+                minLength: {
+                  value: 5,
+                  message: "Password must be at least 5 characters",
+                },
+              }}
+              control={control}
+              render={({ field, fieldState }) => (
+                <Field
+                  {...field}
+                  type="password"
+                  error={errors[field.name]?.message}
+                  placeholder="Enter your password"
+                  size="sm"
+                />
+              )}
+            />
+          </div>
+          <Button
+            appearance="primary"
+            type="submit"
+            className={styles.submitButton}
+          >
+            {isLogin ? "Sign In" : "Sign Up"}
+          </Button>
+        </Form>
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={onClose} appearance="primary">
-          Ok
-        </Button>
-        <Button onClick={onClose} appearance="subtle">
-          Cancel
-        </Button>
-      </Modal.Footer>
     </Modal>
   );
 };
